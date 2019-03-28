c96f553cbd2a7a2c4596c3471aa7d226731ff4d4
k.junki <jyunli5088@gmail.com>
2019-03-28 13:55:25 +0900

    テスト修正

--- tests/TestCase/S3Test.php
+++ tests/TestCase/S3Test.php
@@ -7,17 +7,35 @@ use Aws\S3\S3Client;
 
 class S3Test extends TestCase
 {
-    public function test_getType()
+    private $client = null;
+    private $source = null;
+
+    public function setUp() :void
     {
+        parent::setUp();
+
         $s3Config = [
-            'key' => TestConfig::ACCESS_KEY_ID,
-            'secret' => TestConfig::SECRET_ACCESS_KEY,
-            'region' => TestConfig::REGION
+            'region' => TestConfig::REGION,
+            'version' => 'latest',
+            'credentials' => [
+                'key' => TestConfig::ACCESS_KEY_ID,
+                'secret'  => TestConfig::SECRET_ACCESS_KEY,
+            ]
         ];
 
-        $client = S3Client::factory($s3Config);
-        $source = new ImginS3Source($client, 'imgin-test-fusic');
+        $this->$client = S3Client::factory($s3Config);
+        $this->$source = new ImginS3Source($this->$client, 'imgin-test-fusic');
+    }
 
-        $this->assertEquals('S3', $source->getType());
+    public function test_getType()
+    {
+        $this->assertEquals('S3', $this->$source->getType());
+    }
+
+    public function test_createObject()
+    {
+        $result = $this->$source->createObject('test-img.png', './tests/tmp/test-img.png');
+
+        $this->assertTrue(true);
     }
 }
