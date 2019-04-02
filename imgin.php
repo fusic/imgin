1636e81830c581a6940516025578108b0928573f
k.junki <jyunli5088@gmail.com>
2019-04-02 10:47:44 +0900

    クラスの参照を明記

--- imgin.php
+++ imgin.php
@@ -56,7 +56,7 @@ if (php_sapi_name() == 'cli') {
         if ($imgin['all']) {
             foreach (glob($rootPath . DS . '*', GLOB_ONLYDIR) as $dirname) {
                 if (preg_match('#/' . $dirRegex . '$#', $dirname)) {
-                    cleardir($dirname);
+                    Util::cleardir($dirname);
                 }
             }
 
@@ -145,7 +145,7 @@ try {
         if (defined('IMGIN_DIR_MODE')) {
             $dirmode = IMGIN_DIR_MODE;
         }
-        $result = mkdirWithDirmode(dirname($resizedImagePath), $dirmode, true);
+        $result = Util::mkdirWithDirmode(dirname($resizedImagePath), $dirmode, true);
         if (!$result) {
             throw new OutOfBoundsException('Directory permission denied');
         }
